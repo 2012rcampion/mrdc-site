217216b62398622671e1dd157a16b0fb51184c55
Robert Campion <2012rcampion@gmail.com>
2016-11-12 11:51:35 -0500

    added new fields to submission php script

--- register_submit.php
+++ register_submit.php
@@ -46,23 +46,18 @@ try {
     throw new Exception('');
   }
   
-  if(is_null($_POST['TeamEmail']) || $_POST['TeamEmail'] == '') {
-    $db_error_msg = 'An email address is required.';
-    throw new Exception('');
-  }
-  
   if(!$agreed) {
     $db_error_msg = 'You must agree to all the terms and conditions.';
     throw new Exception('');
   }
   
-  $sql = 'INSERT INTO RegistrationTeams (Name, Abbr, School, Email, Agreed) VALUE (?, ?, ?, ?, ?)'; 
-  $pdo->prepare($sql)->execute([$_POST['TeamName'], $_POST['TeamAbbr'], $_POST['TeamSchool'], $_POST['TeamEmail'], $agreed]);
+  $sql = 'INSERT INTO RegistrationTeams (Name, Abbr, School, Agreed) VALUE (?, ?, ?, ?)'; 
+  $pdo->prepare($sql)->execute([$_POST['TeamName'], $_POST['TeamAbbr'], $_POST['TeamSchool'], $agreed]);
 
   $sql = 'SET @TeamId = LAST_INSERT_ID()';
   $pdo->prepare($sql)->execute();
 
-  $sql = 'INSERT INTO RegistrationMembers (Name, TeamId, Type, ShirtSize) VALUE (?, @TeamId, ?, ?)';
+  $sql = 'INSERT INTO RegistrationMembers (Name, TeamId, Type, Email, Phone, ShirtSize) VALUE (?, @TeamId, ?, ?, ?, ?)';
   $statement = $pdo->prepare($sql);
 
   foreach(range(1, 6) as $i) {
@@ -71,7 +66,18 @@ try {
         $db_error_msg = "A t-shirt size is missing for team member ${i}.";
         throw new Exception('');
       }
-      $statement->execute([$_POST["Member${i}Name"], $i == 1 ? 'Captain' : 'Member', $_POST["Member${i}Size"]]);
+      
+      if(is_null($_POST["Member${i}Email"]) || $_POST["Member${i}Email"] == '') {
+        $db_error_msg = "An email address is missing for team member ${i}.";
+        throw new Exception('');
+      }
+      
+      if(is_null($_POST["Member${i}Phone"]) || $_POST["Member${i}Phone"] == '') {
+        $db_error_msg = "A phone number is missing for team member ${i}.";
+        throw new Exception('');
+      }
+      
+      $statement->execute([$_POST["Member${i}Name"], $i == 1 ? 'Captain' : 'Member', $_POST["Member${i}Email"], $_POST["Member${i}Phone"], $_POST["Member${i}Size"]]);
     }
     else {
       if($i == 1) {
@@ -86,12 +92,24 @@ try {
       $db_error_msg = "A t-shirt size is missing for the team sponsor.";
       throw new Exception('');
     }
-    $statement->execute([$_POST['SponsorName'], 'Sponsor', $_POST['SponsorSize']]);
+    
+    if(is_null($_POST["SponsorEmail"]) || $_POST["SponsorEmail"] == '') {
+      $db_error_msg = "An email address is missing for the team sponsor.";
+      throw new Exception('');
+    }
+    
+    if(is_null($_POST["SponsorPhone"]) || $_POST["SponsorPhone"] == '') {
+      $db_error_msg = "A phone number is missing for the team sponsor.";
+      throw new Exception('');
+    }
+    
+    $statement->execute([$_POST['SponsorName'], 'Sponsor', $_POST['SponsorEmail'], $_POST['SponsorPhone'], $_POST['SponsorSize']]);
   }
   $pdo->commit();
 
 }
 catch(Exception $e) {
+  file_put_contents('error.log', $e, FILE_APPEND);
   $pdo->rollBack();
   $db_problem = true;
 }
